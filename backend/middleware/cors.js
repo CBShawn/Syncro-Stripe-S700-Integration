7643d052bdee7fef24a2e737561a1621bde5c3b1
CBShawn <shawn@codeblackit.com>
2026-08-05 19:16:36 -0400

    Add CORS middleware with configuration options
    Set up CORS middleware with specific configurations.

--- backend/middleware/cors.js
+++ backend/middleware/cors.js
@@ -1,1 +1,20 @@
+const cors = require("cors");
 
+module.exports = cors({
+  origin: true,
+  methods: [
+    "GET",
+    "POST",
+    "PUT",
+    "DELETE",
+    "OPTIONS"
+  ],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "x-extension-auth",
+    "x-extension-key",
+    "x-requested-with",
+  ],
+  credentials: true,
+});
