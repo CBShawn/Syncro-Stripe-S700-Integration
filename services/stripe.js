7352d843052cadc1873e74c0c4a3180291e8cdaf
CBShawn <shawn@codeblackit.com>
2026-08-05 16:04:56 -0400

    Refactor Stripe module export syntax

--- services/stripe.js
+++ services/stripe.js
@@ -1,7 +1,6 @@
 const Stripe = require("stripe");
-
 const config = require("../config");
 
-module.exports = new Stripe(
-    config.STRIPE_SECRET_KEY
+module.exports = Stripe(
+  config.STRIPE_SECRET_KEY
 );
