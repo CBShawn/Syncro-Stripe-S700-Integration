42a0f3ed502cf24fc62125ccf77d7c5d2850251e
CBShawn <shawn@codeblackit.com>
2026-08-05 17:21:36 -0400

    Add payment service module with initial setup

---
+++ backend/services/payment.js
@@ -0,0 +1,15 @@
+const axios = require("axios");
+
+const {
+  invoicePaymentStatus,
+  processedSyncroPayments,
+} = require("./cache");
+
+const PORT = process.env.PORT || 3000;
+const SYNCRO_SUBDOMAIN = process.env.SYNCRO_SUBDOMAIN;
+const SYNCRO_API_KEY = process.env.SYNCRO_API_KEY;
+
+module.exports = {
+  recordSyncroPayment,
+  clearTerminalReaderDisplay,
+};
