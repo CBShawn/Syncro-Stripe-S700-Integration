de4e082e4db48613435d20324894c97a2b7a4596
CBShawn <shawn@codeblackit.com>
2026-08-05 19:32:03 -0400

    Update terminal.js

--- backend/routes/terminal.js
+++ backend/routes/terminal.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 
 const { setTerminalReaderDisplay } = require("../services/terminalService");
 const stripe = require("../services/stripe");
