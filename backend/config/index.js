cea1ae9d032bc191c4895c9e27122c3186292764
CBShawn <shawn@codeblackit.com>
2026-08-05 16:24:10 -0400

    Configure environment variables in index.js
    Added environment variable configurations for the application.

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,1 +1,23 @@
+require("dotenv").config();
 
+module.exports = {
+  PORT: process.env.PORT || 3000,
+
+  STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+
+  STRIPE_WEBHOOK_SECRET:
+    process.env.STRIPE_WEBHOOK_SECRET,
+
+  SYNCRO_SUBDOMAIN:
+    process.env.SYNCRO_SUBDOMAIN,
+
+  SYNCRO_API_KEY:
+    process.env.SYNCRO_API_KEY,
+
+  DEFAULT_STRIPE_READER_ID:
+    process.env.DEFAULT_STRIPE_READER_ID,
+
+  BASE_URL:
+    process.env.RENDER_EXTERNAL_URL ||
+    process.env.BASE_URL
+};
