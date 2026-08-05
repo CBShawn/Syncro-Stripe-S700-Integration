0c616398118becf4daaa12a12e7884be4a0b50ef
CBShawn <shawn@codeblackit.com>
2026-08-05 16:23:50 -0400

    Initialize cache structures in index.js

--- backend/cache/index.js
+++ backend/cache/index.js
@@ -1,1 +1,13 @@
+module.exports = {
 
+  invoiceCustomerCache: new Map(),
+
+  invoicePaymentStatus: new Map(),
+
+  processedSyncroPayments: new Set(),
+
+  pendingSyncroPayments: new Map(),
+
+  activeReaderIntents: new Map()
+
+};
