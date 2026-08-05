4f6a62121cfaedf4bbf912e8116b7b0f96ea67ea
CBShawn <shawn@codeblackit.com>
2026-08-05 19:21:15 -0400

    Add custom body parser middleware for JSON requests

--- backend/middleware/bodyParser.js
+++ backend/middleware/bodyParser.js
@@ -1,1 +1,15 @@
+const express = require("express");
 
+module.exports = (req, res, next) => {
+  if (req.originalUrl === "/api/stripe/webhook") {
+    return next();
+  }
+
+  express.json({
+    type: [
+      "application/json",
+      "text/plain",
+      "application/x-www-form-urlencoded",
+    ],
+  })(req, res, next);
+};
