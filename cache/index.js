19ffce39484598fabf0a1fe8fa3feca72c25ea0f
CBShawn <shawn@codeblackit.com>
2026-08-05 14:37:33 -0400

    Initialize cache structures for invoice processing

--- cache/index.js
+++ cache/index.js
@@ -1,1 +1,19 @@
+const invoiceCustomerCache = new Map();
 
+const invoicePaymentStatus = new Map();
+
+const processedSyncroPayments = new Set();
+
+const pendingSyncroPayments = new Map();
+
+module.exports = {
+
+    invoiceCustomerCache,
+
+    invoicePaymentStatus,
+
+    processedSyncroPayments,
+
+    pendingSyncroPayments
+
+};
