fd89f4d0df61ef6457dc5ed8c4c0666567ae746c
CBShawn <shawn@codeblackit.com>
2026-08-05 20:04:26 -0400

    Refactor imports from terminalService for clarity

--- backend/routes/terminal.js
+++ backend/routes/terminal.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
-const { setTerminalReaderDisplay } = require("../services/terminalService");
+const { 
+  setTerminalReaderDisplay,
+  setReaderMessage
+} = require("../services/terminalService");
 const stripe = require("../services/stripe");
 const syncro = require("../services/syncroService");
 
