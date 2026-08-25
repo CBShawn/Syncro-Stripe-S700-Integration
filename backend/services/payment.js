65cfb47405dc831a5e7cc14a4b52f2b1dc7ed037
CBShawn <shawn@codeblackit.com>
2026-08-25 14:45:07 -0400

    Rename 'note' to 'message' in payment object

--- backend/services/payment.js
+++ backend/services/payment.js
@@ -211,7 +211,7 @@ async function recordSyncroPayment(
         payment_method: finalPaymentMethod,
         ref_num: referenceString,
         ip_address: resolvedClientIp || "",
-        note: notestring,
+        message: notestring,
         transaction_response: transactionresponse,
         address_state: customerData.state || "",
         address_street: customerData.address || "",
